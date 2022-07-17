e71a675a98834c51a331069f7a73f2fd9cbacca1
Daniel Ehrhardt <daniel.ehrhardt@codext.de>
2022-07-17 18:07:04 +0200

    Added forever

--- pm2.config.js
+++ pm2.config.js
@@ -3,7 +3,15 @@ module.exports = {
         {
             name: 'restreamer',
             script: 'dist/index.js',
-            args: '--rtmp=rtmp://a.rtmp.youtube.com/live2/4x4m-1sxe-syrh-4y6u-dmm0 --url=https://www.windy.com/'
+            args: '--rtmp=rtmp://a.rtmp.youtube.com/live2/4x4m-1sxe-syrh-4y6u-dmm0 --url=https://www.windy.com/',
+            instances: "max",
+            max_memory_restart: "2G",
+            env: {
+                NODE_ENV: "development",
+            },
+            env_production: {
+                NODE_ENV: "production",
+            }
         },
     ],
 };
