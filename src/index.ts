0a13a24a2f05c1cc62e64d019db294c43a418675
Daniel Ehrhardt <daniel.ehrhardt@codext.de>
2022-07-17 16:15:19 +0200

    Added Custom resolution

--- src/index.ts
+++ src/index.ts
@@ -8,35 +8,53 @@ puppeteer.use(StealthPlugin());
 const streamToRtmp = async (
   rtmpUrl: string,
   pageUrl: string,
-  resolution?: { width: number; height: number }
+  resolution: { width: number; height: number } = { width: 1280, height: 720 }
 ) => {
   const browser = await launch({
     defaultViewport: null,
     args: [
+      "--no-zygote",
       "--no-sandbox",
-      "--disable-setuid-sandbox",
-      `--window-size=${resolution.width || 1920},${resolution.height || 1080}`,
+     // "--disable-gpu",
+     // "--single-process",
+     // "--start-fullscreen",
       "--headless=chrome",
+      "--ignore-certificate-errors",
+      "--disable-software-rasterizer",
+      "--disable-setuid-sandbox",
+      `--window-size=${resolution.width},${resolution.height}`,
     ],
   });
 
   const page = await browser.newPage();
   await page.goto(pageUrl);
 
+  const videoConstraints = {
+    mandatory: {
+      minWidth: resolution.width,
+      minHeight: resolution.height,
+      maxWidth: resolution.width,
+      maxHeight: resolution.height,
+    },
+  };
+
   const stream = await getStream(page, {
     audio: true,
     video: true,
+    frameSize: 1000,
+    // @ts-ignore
+    videoConstraints,
   });
 
   console.log("restreaming to", rtmpUrl);
 
   const ffmpeg = exec(
-    `ffmpeg -i - -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -f flv ${rtmpUrl}`
+    `ffmpeg -i - -c:v libx264 -vf scale=${resolution.width}:${resolution.height}  -preset veryfast -tune zerolatency -c:a aac -f flv ${rtmpUrl}`
     // `ffmpeg -i -  -map 0 -c:v libx264 -vf scale=640:480 -preset veryfast -tune zerolatency -g:v 60 -c:a aac -strict -2 -ar 44100 -b:a 64k -y -use_wallclock_as_timestamps 1 -async 1 -flags +global_header -f flv ${rtmpUrl}`
   );
 
   ffmpeg.stderr?.on("data", (chunk) => {
-    // console.log(chunk.toString());
+    console.log(chunk.toString());
   });
 
   stream.pipe(ffmpeg.stdin!!);
